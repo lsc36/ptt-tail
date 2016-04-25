4fd076a64a3686aca75ad4f034508cddfd091bf7
Shao-Chuan Lee <lsc36x@gmail.com>
2016-04-25 17:28:04 +0800

    stronger regex_match to replace keyword_match

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
-"""Decorator for hooks to activate if message matches any of given keywords."""
-def keyword_match(*kwlist):
+import re
+
+
+"""Decorator for hooks to activate if message matches any of given regex."""
+def regex_match(*rlist):
     def _deco(func):
         def _wrap(time, ptype, user, msg):
-            if any(kw in msg for kw in kwlist):
+            if any(re.search(r, msg) is not None for r in rlist):
                 return func(time, ptype, user, msg)
         return _wrap
     return _deco
